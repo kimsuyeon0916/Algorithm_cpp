a4b321611edaba2c0cd969fc93eeb360690af291
kimsuyeon0916 <5667649@naver.com>
2023-07-02 20:29:52 +0900

    2 x n 타일링

--- Baekjoon/Dynamic Programming/11726.cpp
+++ Baekjoon/Dynamic Programming/11726.cpp
@@ -7,7 +7,7 @@ int dp[1001];
 int main(){
   int n; cin >> n;
   
-  dp[1]=1; dp[2]=2;
+  dp[1]=1; dp[2]=2; //boundary condition
   for(int i=3;i<=1000;i++) dp[i]=((1*dp[i-1])+(1*dp[i-2]))%10007; //dp[i-2]에서 dp[i-1]랑 겹치는거 제외한 경우
   
   cout << dp[n];
