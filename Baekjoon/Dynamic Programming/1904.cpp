74954e03afbc207fa8ea857f220d7e61836e56c2
kimsuyeon0916 <5667649@naver.com>
2023-07-02 20:20:59 +0900

    01타일

---
+++ Baekjoon/Dynamic Programming/1904.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[1000001]={0,};
+int main(){
+  int n; cin >> n;
+
+  dp[1]=1; dp[2]=2; //boundary condition
+  for(int i=3;i<=n;i++) dp[i]=(dp[i-1]+dp[i-2])%15746;
+
+  cout << dp[n]; return 0;
+}
