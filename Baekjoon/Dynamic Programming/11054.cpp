701ad34996b1e78765c9f1cdcb093e09a3fa9cda
kimsuyeon0916 <5667649@naver.com>
2023-07-11 20:20:20 +0900

    가장 긴 바이토닉 부분 수열

--- Baekjoon/Dynamic Programming/11054.cpp
+++ Baekjoon/Dynamic Programming/11054.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
-
+#define FASTIO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
 using namespace std;
 
-void init(){
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  cout.tie(0);
-}
-
-int dp[1001][3]={0,};
+int dp[1001][2]={0,};
 int input[1001];
 
 int main(){
-  init();
+  FASTIO;
   int n; cin >> n;
   for(int i=1;i<=n;i++) cin >> input[i];
 
-  int result=0;
-  dp[1][0]=1; dp[1][1]=1; dp[1][2]=1; //boundary condition
+  int plus=1; //순방향 증가 수열
   for(int i=1;i<=n;i++){
+    dp[i][0]=1; //boundary condition
     for(int j=1;j<i;j++){
       if(input[i]>input[j]) dp[i][0]=max(dp[i][0],dp[j][0]+1);
-      if(input[i]<input[j]){
-        dp[i][1]=max(dp[i][1],dp[j][1]+1);
-        dp[i][2]=max(dp[i][2],(dp[j][0]+(dp[i][1]-dp[j][1])));
-      } 
     }
-    result=max(max(dp[i][0],dp[i][1]),max(dp[i][2],result));
-    cout << dp[i][0] << " " << dp[i][1] << " " << dp[i][2] << "\n";
+    plus=max(dp[i][0],plus);
   }
 
+  int minus=1; //역방향 증가 수열
+  for(int i=n;i>=1;i--){
+    dp[i][1]=1; //boundary condition
+    for(int j=n;j>i;j--){
+      if(input[i]>input[j]) dp[i][1]=max(dp[i][1],dp[j][1]+1);
+    }
+    minus=max(dp[i][1],minus);
+  }
+
+  int result=1; //순방향 증가 수열 + 역방향 증가 수열
+  for(int i = 1; i<=n; i++){
+		result = max(result, dp[i][0] + dp[i][1] -1);
+	}
   cout << result;
   return 0;
 }
-
-//이 풀이는 안됨
