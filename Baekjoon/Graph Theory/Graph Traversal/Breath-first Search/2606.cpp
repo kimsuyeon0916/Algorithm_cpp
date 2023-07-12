d405a92519f61abd0ae317859cc1e38ef2ce578c
kimsuyeon0916 <5667649@naver.com>
2023-07-12 10:24:26 +0900

    바이러스

---
+++ Baekjoon/Graph Theory/Graph Traversal/Breath-first Search/2606.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+void BFS(int start);
+
+vector<int> graph[101];
+bool visited[101]={0,};
+int num=-1;
+int main()
+{
+  FASTIO;
+  int N, M;
+  cin >> N >> M;
+  int u, v;
+  for(int i=1;i<=M;i++){
+    cin >> u >> v;
+    graph[u].emplace_back(v);
+    graph[v].emplace_back(u);
+  }
+  
+  for(int i=1;i<=N;i++) sort(graph[i].begin(),graph[i].end());
+  BFS(1);
+
+  cout << num;
+  return 0;
+}
+
+void BFS(int start){
+  queue<int> q;
+  q.emplace(start);
+  visited[start]=true;
+  ++num;
+  while (!q.empty()){
+    int cur=q.front();
+    q.pop();
+    for(int v:graph[cur]){
+      if(!visited[v]){
+        q.emplace(v);
+        visited[v]=true;
+        ++num;
+      }
+    }
+  }
+}
