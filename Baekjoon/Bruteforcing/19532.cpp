971ac552fdbef04a68c9e03fcc1d3922281d6d53
kimsuyeon0916 <5667649@naver.com>
2023-07-14 23:55:41 +0900

    수학은 비대면강의입니다

--- Baekjoon/Bruteforcing/19532.cpp
+++ Baekjoon/Bruteforcing/19532.cpp
@@ -11,9 +11,22 @@ int main()
   int x, y;
   cin >> a >> b >> c >> d >> e >> f;
 
+  /* 수식 전개 */
   x=(c*e-f*b)/(a*e-d*b);
   y=(c*d-f*a)/(b*d-e*a);
-  
+
+  /* 완전 탐색 */
+  for (int i = -999; i < 1000; i++) {
+		for (int j = -999; j < 1000; j++) {
+			if (a * i + b * j == c) {
+				if (d * i + e * j == f) {
+					cout << i << " " << j;
+					return 0;
+				}
+			}
+		}
+	}
+
   cout << x << " " << y;
   return 0;
 }
