2abf762c2e4513f528988bbf596af59997c7a1e6
kimsuyeon0916 <5667649@naver.com>
2023-07-15 01:08:05 +0900

    분해합

---
+++ Baekjoon/Bruteforcing/2231.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int answer = 0;
+    for (int i = 1; i < N; i++) {
+        int num = i;
+        int sum = num;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        if (sum == N) {
+            answer = i;
+            break;
+        }
+    }
+
+    cout << answer << endl;
+
+    return 0;
+}
