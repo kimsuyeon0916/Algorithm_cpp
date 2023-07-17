7b3a92756f2ad3ccb2c3cbabd2221f18f9424420
kimsuyeon0916 <5667649@naver.com>
2023-07-17 23:04:12 +0900

    이분 그래프

--- Baekjoon/Graph Theory/Graph Traversal/1707.cpp
+++ Baekjoon/Graph Theory/Graph Traversal/1707.cpp
@@ -5,69 +5,54 @@
 #define FASTIO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
 using namespace std;
 
-void BFS(int start);
-void DFS(int v);
+void DFS(int parent,int v);
 
 vector<int> graph[20001];
 int visited[20001];
-
 int main()
 {
   FASTIO;
-  int K;
-  cin >> K;
-  
-  for (int i = 1; i <= K; i++) {
-    int V, E;
-    string result = "YES";
-    fill_n(visited, 20001, 0); // 테케마다 초기화
-    for (int i = 0; i <= 20000; i++) graph[i].clear();
-
+  int K; cin >> K;
+  int V,E;
+  for(int i=1;i<=K;i++){
+    string result="YES";
+    fill_n(visited,20001,0); //테케마다 초기화
+    for(int i=0;i<=20000;i++) graph[i].clear();
     cin >> V >> E;
     int u, v;
-    for (int i = 1; i <= E; i++) {
+    for(int i=1;i<=E;i++){
       cin >> u >> v;
       graph[u].emplace_back(v);
       graph[v].emplace_back(u);
     }
+    for(int i=1;i<=V;i++) sort(graph[i].begin(), graph[i].end());
 
-    for (int i = 1; i <= V; i++)
-      sort(graph[i].begin(), graph[i].end());
-
-    for (int i = 1; i <= V; i++) { // 모든 정점 확인해야 함
-      if (visited[i] == 0) {
-        visited[i] = 1;
-        DFS(i);
+    visited[0]=-1;
+    for(int i=1;i<=V;i++){ //모든 정점 확인해야 함
+      if(visited[i]==0){
+        DFS(0,i);
       }
     }
-
-    for (int i = 1; i <= V; i++) {
-      for (int vertex : graph[i]) {
-        if (visited[i] == visited[vertex]) {
+ 
+    for(int i=1;i<=V;i++){
+      for(int vertex:graph[i]){
+        if(visited[vertex]==visited[i]){ //처음에 i대신 복붙한 그대로 적어서 계속 틀림;;
           result = "NO";
           break;
-        }
+        } 
       }
-      if (result == "NO")
-        break;
+      if(result=="NO") break;
     }
     cout << result << '\n';
   }
   return 0;
 }
 
-void BFS(int start)
-{
-  queue<int> q;
-  q.emplace(start);
-}
-
-void DFS(int v)
-{
-  for (int vertex : graph[v]) {
-    if (visited[vertex] == 0) {
-      visited[vertex] = -visited[v];
-      DFS(vertex);
+void DFS(int parent,int v){
+  visited[v]=-visited[parent];
+  for(int vertex:graph[v]){
+    if(!visited[vertex]){
+      DFS(v,vertex);
     }
   }
 }
