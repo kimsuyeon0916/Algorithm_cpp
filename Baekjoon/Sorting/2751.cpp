211d2d694aec6ea6ab44eb47b60636cbf94a41c1
kimsuyeon0916 <5667649@naver.com>
2023-07-19 18:29:52 +0900

    수 정렬하기 2

---
+++ Baekjoon/Sorting/2751.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main()
+{
+  FASTIO;
+  int N; cin >> N;
+
+  vector<int> v(N);
+  for(int i=0;i<N;i++){
+    cin >> v[i];
+  }
+
+  sort(v.begin(),v.end());
+  for(int x : v) cout << x << '\n';
+  return 0;
+}
