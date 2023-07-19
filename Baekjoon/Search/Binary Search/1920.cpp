c85b45738c2134f30dd3c791ef60c408284af533
kimsuyeon0916 <5667649@naver.com>
2023-07-20 00:28:13 +0900

    수 찾기

---
+++ Baekjoon/Search/Binary Search/1920.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main()
+{
+  FASTIO;
+  int N; cin >> N;
+  vector<int> v(N);
+  for(int i=0;i<N;i++) cin >> v[i];
+  sort(v.begin(),v.end());
+  int M; cin >> M;
+  for(int i=0;i<M;i++){
+    int input,res=0; cin >> input;
+    int low=0, high=N-1; //while 바로 위에 있어야 하는게 맞음!
+    while(low<=high){
+      int mid=(low+high)/2; //소괄호 필수 & while문 안에
+      if(input==v[mid]){
+        res=1;
+        break;
+      } 
+      else if(input>v[mid]) low=mid+1;
+      else high=mid-1;
+    }
+    cout << res <<'\n';
+  }
+  return 0;
+}
