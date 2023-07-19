e6cebc68196fafa7fd18f55a20b7a690a9af71e8
kimsuyeon0916 <5667649@naver.com>
2023-07-19 21:36:29 +0900

    평균

--- Baekjoon/Implementation/Mathematics/1546.cpp
+++ Baekjoon/Implementation/Mathematics/1546.cpp
@@ -8,19 +8,14 @@ int main()
 {
   FASTIO;
   int N; cin >> N;
-  long double max_score=0,s;
-  vector<long double> score(N);
+  long double max_score=0,average=0,s;
   for(int i=0;i<N;i++){
     cin >> s;
-    score[i]=s;
+    average+=s;
     max_score=max(s,max_score);
   }
-  long double average=0;
-  for(long double n:score){
-    n=n/max_score*100;
-    average+=n;
-  }
-  average/=N;
+  average=average/max_score*100/N;
+
   cout << average;
   return 0;
 }
