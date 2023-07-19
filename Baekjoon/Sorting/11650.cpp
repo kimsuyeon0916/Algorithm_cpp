10826a314450d778e6b0deebb0b7d0b48ce1d411
kimsuyeon0916 <5667649@naver.com>
2023-07-19 18:30:05 +0900

    좌표 정렬하기

--- Baekjoon/Sorting/11650.cpp
+++ Baekjoon/Sorting/11650.cpp
@@ -13,12 +13,14 @@ int main()
 {
   FASTIO;
   int N; cin >> N;
+  
   vector<pair<int,int>> v; //크기 설정 안해놔서, 인덱스 접근 X
   int x,y;
   for(int i=0;i<N;i++){
     cin >> x >> y;
     v.push_back({x,y}); //v.emplace_back(make_pair(x,y));
   }
+
   sort(v.begin(),v.end(),comp);
   for(pair<int,int> xy:v) cout << xy.first << " " << xy.second << '\n';
   return 0;
