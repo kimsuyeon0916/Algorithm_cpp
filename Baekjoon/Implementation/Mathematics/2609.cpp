f43097ba1f98d7a1bd753f2d5aba56f51e63be8e
kimsuyeon0916 <5667649@naver.com>
2023-07-20 13:09:24 +0900

    최대공약수와 최소공배수

--- Baekjoon/Implementation/Mathematics/2609.cpp
+++ Baekjoon/Implementation/Mathematics/2609.cpp
@@ -10,14 +10,35 @@ int main()
   int n1, n2; cin >> n1 >> n2;
 
   int divisor=0, multiple=0;
-  for(int i=max(n1,n2);i>=1;i--){
+  for(int i=max(n1,n2);i>=1;i--){ 
     if(n1%i==0 && n2%i==0){
       divisor=i;
       break;
     }
   }
-  multiple=divisor*(n1/divisor)*(n2/divisor);
-  
+  multiple=divisor*(n1/divisor)*(n2/divisor); //=(n1*n1)/divisor
+
   cout << divisor << '\n' << multiple;
   return 0;
 }
+
+//유클리드 호제법
+/*
+int gcd(int a, int b) { //최대 공약수
+	int r = a % b; //a가 큰 수, b가 작은 수
+	while (r != 0) {
+		a = b; 
+		b = r; 
+		r = a % b; 
+	}
+	return b;
+}
+int gcd(int a, int b) {
+	if (b == 0) return a;
+	return gcd(b, a%b);
+}
+
+int lcm(int a, int b) { //최소 공배수
+	return (a * b) / gcd(a, b);
+}
+*/
