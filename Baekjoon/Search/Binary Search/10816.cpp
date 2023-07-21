cd246e0a3bc65827362f94f6963a2eb7a956fd83
kimsuyeon0916 <5667649@naver.com>
2023-07-21 11:06:14 +0900

    숫자 카드 2

---
+++ Baekjoon/Search/Binary Search/10816.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main()
+{
+  FASTIO;
+  int N; cin >> N;
+  vector<int> v(N);
+  for(int i=0;i<N;i++) cin >> v[i];
+  sort(v.begin(),v.end());
+  int M; cin >> M;
+  for(int i=0;i<M;i++){
+    int x;  cin >> x;
+    int count=0;
+    count=equal_range(v.begin(),v.end(),x).second-equal_range(v.begin(),v.end(),x).first;
+    cout << count << " ";
+  }
+  
+  return 0;
+}
