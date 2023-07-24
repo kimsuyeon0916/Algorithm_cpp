985afa58e956e0e35971763c8c2ae23c8010d15b
kimsuyeon0916 <5667649@naver.com>
2023-07-24 11:22:28 +0900

    균형잡힌 세상

---
+++ Baekjoon/Data Structures/Stack/4949.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+#include <algorithm>
+#include <string>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main()
+{
+  FASTIO;
+  string input;
+  while(getline(cin,input,'.')){ //'.' 이전까지의 문자열(한 줄)을 str에 통째로 입력받음
+    cin.ignore();
+    stack<char> s; //왼쪽 괄호만 들어있는 스택
+    string result="yes";
+    for(char c:input){
+      if(c=='['||c=='(') s.emplace(c);
+      else if(c==')'){
+        if(s.empty()){
+          result="no";
+          break;
+        }
+        else{
+          if(s.top()=='(') s.pop();
+          else{
+            result="no";
+            break;
+          }
+        }
+      }
+      else if(c==']'){
+        if(s.empty()){
+          result="no";
+          break;
+        }
+        else{
+          if(s.top()=='[') s.pop();
+          else{
+            result="no";
+            break;
+          }
+        }
+      }
+    }
+    if(!s.empty()) result="no";
+    if(input.size()) cout << result << '\n'; //.은 제외
+  }
+  return 0;
+}
