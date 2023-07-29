798b188c61d7741d044f888d5753550b21443771
kimsuyeon0916 <5667649@naver.com>
2023-07-30 02:59:27 +0900

    잃어버린 괄호호

--- Baekjoon/Greedy/1541.cpp
+++ Baekjoon/Greedy/1541.cpp
@@ -10,16 +10,16 @@ int main()
   int n;
   char c;
   cin >> n;
-  int part=0, minus_cnt=0, sum=n;
+  int part=0, minus_cnt=0, sum=n; //part는 부분합을 의미하는 변수
   while(cin >> c >> n){ //EOI
     if(c=='-'){
-      if(minus_cnt) sum-=part;
-      else sum+=part;
+      if(!minus_cnt) sum+=part; //-가 처음 나온 경우
+      else sum-=part; //-가 2번 이상 나온 경우
       ++minus_cnt;
       part=0;
     } 
     part+=n;
   }
-  cout << (minus_cnt ? sum-part : sum+part);
+  cout << (minus_cnt ? sum-part : sum+part); //마지막 부분합을 처리
   return 0;
 }
