0aa5869a81e477b2ed9f1f9bd924fe0adc1595a6
kimsuyeon0916 <5667649@naver.com>
2023-07-29 20:29:57 +0900

    회의실 배정

---
+++ Baekjoon/Greedy/1931.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+bool cmp(pair<int,int> a, pair<int,int> b){
+  if(a.second==b.second) return a.first < b.first; //오름차순
+  return a.second < b.second; //오름차순
+}
+
+int main() {
+  FASTIO;
+  int N; cin >> N; 
+  int max_end = 0, max_cnt = 0;
+  vector<pair<int,int>> period;
+  for (int i = 0; i < N; i++) {
+    int s, e; cin >> s >> e;
+    period.emplace_back(make_pair(s,e));
+  }
+  sort(period.begin(), period.end(),cmp);
+  int cnt = 1;
+  int next=period[0].second;
+  for(int idx=1;idx<N;idx++){
+    if(period[idx].first>=next){ //같지 않을 수 있음
+      ++cnt;
+      next=period[idx].second;
+    } 
+  }
+  cout << cnt;
+  return 0;
+}
