ca1ee3cb16f62935b2b60cedd1b722f087607f55
kimsuyeon0916 <5667649@naver.com>
2023-07-30 01:35:10 +0900

    ATM

--- Baekjoon/Greedy/11399.cpp
+++ Baekjoon/Greedy/11399.cpp
@@ -12,8 +12,8 @@ int main()
   for(int i=0;i<N;i++) cin >> p[i];
   sort(p.begin(),p.end());
   int sum=0, time=0;
-  for(int i=0;i<N;i++){
-    time+=p[i];
+  for(int i=0;i<N;i++){ 
+    time+=p[i]; //sum += p[i]*(n-i); 대기시간 반복수만큼 해당 p를 더해줌
     sum+=time;
   } 
   cout << sum;
