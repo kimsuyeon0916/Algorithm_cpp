5a9f3956df144bebd780c657d26c64bd4ff2a996
kimsuyeon0916 <5667649@naver.com>
2023-07-31 01:57:06 +0900

    곱셈

--- Baekjoon/Divide And Conquer/1629.cpp
+++ Baekjoon/Divide And Conquer/1629.cpp
@@ -8,13 +8,13 @@ int main()
 {
   long long A, B, C;
   cin >> A >> B >> C;
-  long long res = 1;
+  long long res = 1; //제곱이라 무조건 long long 으로
   while (B > 0)
   {
-    if (B % 2 == 1)
-        res = (res * A) % C;
-    A = ((A % C) * (A % C)) % C;
-    B /= 2;
+    if (B % 2 == 1) //제곱수가 홀수인 경우
+        res = (res * A) % C; //한번 더 곱해줌
+    A = ((A % C) * (A % C)) % C; 
+    B /= 2; //log B번 하기 위해서 B를 줄여나간다는 개념 -> 시간 복잡도 : log B
   }
   cout << res;
 }
