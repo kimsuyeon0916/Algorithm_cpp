473a853a5fa91bbb5797c928b87a029001f49560
kimsuyeon0916 <5667649@naver.com>
2023-08-03 02:24:24 +0900

    좌표 압축

--- Baekjoon/Sorting/18870.cpp
+++ Baekjoon/Sorting/18870.cpp
@@ -15,6 +15,7 @@ int main()
     v[i].second=i; //입력 순서
   }
   v[N].first=MAX_INPUT; v[N].second=v[N-1].second+1;
+  
   sort(v.begin(),v.end()); //크기 순 정렬
   int rank=0; 
   for(int i=1;i<=N;i++){ //N번째 인덱스 포함
@@ -24,6 +25,7 @@ int main()
     v[i-1].second=(flag ? rank++ : rank); //크기 별 인덱스
   }
   sort(v.begin(),v.end()-1); //입력 순서 순 정렬
+  
   for(int i=0;i<N;i++) cout << v[i].second << ' ';
   return 0;
 }
