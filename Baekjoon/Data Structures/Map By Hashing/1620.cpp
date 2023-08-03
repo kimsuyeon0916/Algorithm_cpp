5311eeafaeac0981361b22f419a754fd5b5e355f
kimsuyeon0916 <5667649@naver.com>
2023-08-03 15:53:28 +0900

    나는야 포켓몬 마스터 이다솜

---
+++ Baekjoon/Data Structures/Map By Hashing/1620.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+#include <string>
+#define FASTIO ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main()
+{
+  FASTIO;
+  int N, M; cin >> N >> M;
+  map<int,string> i_s;
+  map<string,int> s_i;
+  for(int i=1;i<=N;i++){
+    string input;
+    cin >> input;
+    i_s.insert({i,input});
+    s_i.insert({input,i});
+  } 
+  for(int i=1;i<=M;i++){
+    string input; cin >> input;
+    if(isdigit(input[0])) cout << i_s[stoi(input)] << '\n';
+    else cout << s_i[input] << '\n';
+  }
+  return 0;
+}
