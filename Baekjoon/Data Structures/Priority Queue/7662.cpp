90f8c719733bb7bfccfc2d1cb8881b992cc6d62e
kimsuyeon0916 <5667649@naver.com>
2023-08-10 00:55:30 +0900

    이중 우서순위 큐

--- Baekjoon/Data Structures/Priority Queue/7662.cpp
+++ Baekjoon/Data Structures/Priority Queue/7662.cpp
@@ -15,7 +15,6 @@ int main()
     priority_queue<int> max_q; //Max Heap
     priority_queue<int, vector<int>, greater<>> min_q; //Min Heap
     map<int,int> repeat;
-    int push_cnt=0, pop_cnt=0;
     for(int i=0;i<k;i++){
       char c; int x;
       cin >> c >> x;
@@ -24,37 +23,28 @@ int main()
         min_q.emplace(x);
         if(repeat.find(x)==repeat.end()) repeat.emplace(x,1);
         else ++repeat[x];
-        ++push_cnt;
       }
       else{
-        if(push_cnt<=pop_cnt) continue; //Q가 비어있는데 적용할 연산이 ‘D’라면 이 연산은 무시
         if(x==-1){
-          while(!repeat[min_q.top()]) min_q.pop(); //뽑기 전 동기화
-          --repeat[min_q.top()];
-          min_q.pop();
+          while(!min_q.empty() && !repeat[min_q.top()]) min_q.pop(); //뽑기 전 동기화
+          if(!min_q.empty()){
+            --repeat[min_q.top()];
+            min_q.pop();
+          }
         } 
         else{
-          while(!repeat[max_q.top()]) max_q.pop(); //뽑기 전 동기화
-          --repeat[max_q.top()];
-          max_q.pop();
+          while(!max_q.empty() && !repeat[max_q.top()]) max_q.pop(); //뽑기 전 동기화
+          if(!max_q.empty()){
+            --repeat[max_q.top()];
+            max_q.pop();
+          }
         } 
-        ++pop_cnt;
       }
     }
-    if(push_cnt==pop_cnt){ //큐가 비어있는 경우
-      cout << "EMPTY" << '\n';
-      continue;
-    }
-    deque<int> intsec;
-    while(!max_q.empty()){
-      if(repeat[max_q.top()]>=1){
-        intsec.emplace_back(max_q.top());
-        --repeat[max_q.top()];
-      } 
-      max_q.pop();
-    }
-    sort(intsec.begin(),intsec.end());
-    cout << intsec.back() << ' ' << intsec.front() << '\n';
+    while(!max_q.empty() && !repeat[max_q.top()]) max_q.pop(); //뽑기 전 동기화
+    while(!min_q.empty() && !repeat[min_q.top()]) min_q.pop(); //뽑기 전 동기화
+    if(max_q.empty()||min_q.empty()) cout<<"EMPTY\n";
+    else cout << max_q.top() << ' ' << min_q.top() << '\n';
   }
   return 0;
 }
