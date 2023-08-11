3286cab3c79adf60eab33a6ef93f1839b1017d46
kimsuyeon0916 <5667649@naver.com>
2023-08-12 01:15:30 +0900

    패션왕 신해빈

--- Baekjoon/Implementation/Mathematics/9375.cpp
+++ Baekjoon/Implementation/Mathematics/9375.cpp
@@ -13,7 +13,7 @@ int main()
   for(int i=1;i<=T;i++){
     int n; cin >> n;
     map<string, int> m; //각 종류 별 옷 개수 저장 (나중에 수식 계산 위함)
-    set<string> s; //각 종류 (중복되지 않게 타입 개수 세기 위함)
+    set<string> s; //각 종류 (중복되지 않게 종류 개수 세기 위함)
     for(int i=1;i<=n;i++){
       string cloth, type;
       cin >> cloth >> type;
